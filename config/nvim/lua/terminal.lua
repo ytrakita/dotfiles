654ded71b054a76c631b0a082f121cedf734e247
ytrakita <81611554+ytrakita@users.noreply.github.com>
2026-01-08 22:45:47 +0900

    nvim: update terminal

--- config/nvim/lua/terminal.lua
+++ config/nvim/lua/terminal.lua
@@ -1,63 +1,92 @@
-local vim = vim
 local api = vim.api
 
 local M = {}
 
-local types = {
-  lazygit = function() vim.fn.jobstart('lazygit', { term = true }) end,
-  terminal = vim.cmd.terminal,
-}
+---@alias terminal.State terminal.Proc[]
 
-for type, fn in pairs(types) do
-  types[type] = { fn = fn }
+---@type terminal.State
+local state = {}
+
+---@class terminal.Proc
+---@field name string
+---@field cmd string|string[]
+---@field buf? integer
+---@field win? integer
+local Proc = {}
+Proc.__index = Proc
+
+---@param cmd string|string[]
+---@return terminal.Proc
+function Proc.new(cmd)
+  local obj = { cmd = cmd }
+  if type(cmd) == 'string' then
+    obj.name = cmd
+    if cmd == '' then
+      obj.name = nil
+      obj.cmd = api.nvim_get_option_value('shell', {})
+    end
+  end
+  return setmetatable(obj, Proc)
 end
 
-local function init_buf(type)
+function Proc:run()
+  vim.fn.jobstart(self.cmd, { term = true })
+end
+
+function Proc:_init_buf()
   local prebuf = api.nvim_get_current_buf()
+  self.buf = api.nvim_create_buf(false, true)
+  api.nvim_set_current_buf(self.buf)
 
-  local bnum = api.nvim_create_buf(false, true)
-  api.nvim_set_current_buf(bnum)
+  self:run()
 
-  types[type].fn()
+  local ft = self.name or 'terminal'
+  api.nvim_set_option_value('filetype', ft, { buf = self.buf })
 
-  api.nvim_set_option_value('filetype', type, { buf = bnum })
   api.nvim_set_current_buf(prebuf)
-
-  return bnum
 end
 
-local function open_float_win(bnum)
-  local height = math.ceil(vim.o.lines * 0.8) - 4
-  local width = math.ceil(vim.o.columns * 0.8)
-  local wnum = api.nvim_open_win(bnum, true, {
-    style = 'minimal',
-    relative = 'editor',
-    height = height,
-    width = width,
-    row = math.ceil((vim.o.lines - height) / 2) - 1,
-    col = math.ceil((vim.o.columns - width) / 2),
-  })
-  return wnum
-end
+---@alias terminal.Winpos 'f'|'s'|'v'
 
-function M.open(type, opt)
-  if not vim.tbl_contains({ 's', 'v', 'f' }, opt) then return end
-
-  local bnum = types[type].buf or init_buf(type)
-
-  if opt == 's' or opt == 'v' then
-    vim.cmd.wincmd(opt)
-    api.nvim_set_current_buf(bnum)
-    types[type].win = api.nvim_get_current_win()
-  elseif opt == 'f' then
-    types[type].win = open_float_win(bnum)
+---@param winpos terminal.Winpos
+function Proc:open(winpos)
+  if not(self.buf and api.nvim_buf_is_valid(self.buf)) then
+    self:_init_buf()
   end
-
+  if winpos == 's' or winpos == 'v' then
+    vim.cmd.wincmd(winpos)
+    api.nvim_set_current_buf(self.buf)
+    self.win = api.nvim_get_current_win()
+  elseif winpos == 'f' then
+    local ui = api.nvim_list_uis()[1]
+    local height = math.ceil(ui.height * 0.8) - 4
+    local width = math.ceil(ui.width * 0.8)
+    self.win = api.nvim_open_win(self.buf, true, {
+      style = 'minimal',
+      relative = 'editor',
+      height = height,
+      width = width,
+      row = math.ceil((ui.height - height) / 2) - 1,
+      col = math.ceil((ui.width - width) / 2),
+    })
+  end
   vim.cmd.startinsert()
+end
 
-  if not types[type].buf then
-    types[type].buf = bnum
+---@param cmd string|string[]
+---@param winpos terminal.Winpos
+function M.open(cmd, winpos)
+  if not vim.tbl_contains({ 'f', 's', 'v' }, winpos) then return end
+
+  for _, proc in ipairs(state) do
+    if proc.name == cmd then
+      return proc:open(winpos)
+    end
   end
+
+  local proc = Proc.new(cmd)
+  proc:open(winpos)
+  table.insert(state, proc)
 end
 
 return M
